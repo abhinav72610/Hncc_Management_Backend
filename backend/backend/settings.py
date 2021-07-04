729d87b694ab2f7bdfe8552cd15d609eca73c22d
Abhinav kumar <abhinav72610@gmail.com>
2021-07-04 14:03:50 +0530

    docs added and readme updated

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -171,3 +171,6 @@ SIMPLE_JWT = {
     'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
     'TOKEN_TYPE_CLAIM': 'token_type',
 }
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema'}
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.schemas import get_schema_view
+from rest_framework.documentation import include_docs_urls
 
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
@@ -28,4 +30,10 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('docs/', include_docs_urls(title='ManagementAPI')),
+    path('schema', get_schema_view(
+        title="BlogAPI",
+        description="API for the Backend",
+        version="1.0.0"
+    ), name='openapi-schema'),
 ]
