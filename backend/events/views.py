6a0e06016a31b9bc24b9cdba8a53e3d09f6e25b4
Abhinav kumar <abhinav72610@gmail.com>
2021-07-10 23:37:02 +0530

    Admin privilliges added

--- backend/events/views.py
+++ backend/events/views.py
@@ -2,8 +2,25 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import Events
 from .serializers import Eventserializer
+from rest_framework import permissions
+
+
+class IsAdminOrReadOnly(permissions.BasePermission):
+
+    def has_permission(self, request, view):
+        # if request.method in ['list', 'retrieve']:
+        #     return True
+
+        # elif request.method in ['create']:
+        #     return request.user.is_authenticated() and request.user.is_admin
+        # # else:
+        # #     return False
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return request.user.is_staff
 
 
 class EventsView(generics.ListCreateAPIView):
+    permission_classes = [IsAdminOrReadOnly]
     queryset = Events.objects.all()
     serializer_class = Eventserializer
--- backend/meets/views.py
+++ backend/meets/views.py
@@ -1,7 +1,25 @@
 from rest_framework import generics
 from .models import Meet, MeetSerializer
+from rest_framework.permissions import IsAdminUser
+from rest_framework import permissions
+
+
+class IsAdminOrReadOnly(permissions.BasePermission):
+
+    def has_permission(self, request, view):
+        # if request.method in ['list', 'retrieve']:
+        #     return True
+
+        # elif request.method in ['create']:
+        #     return request.user.is_authenticated() and request.user.is_admin
+        # # else:
+        # #     return False
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return request.user.is_staff
 
 
 class MeetView(generics.ListCreateAPIView):
+    permission_classes = [IsAdminOrReadOnly]
     queryset = Meet.objects.all()
     serializer_class = MeetSerializer
